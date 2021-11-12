783c94a16dd428118a1768641dbac7b0805cdc69
Metehan Bolat <metehan.bolat.ie@gmail.com>
2021-11-12 19:41:18 +0300

    Navigation created.

--- app/src/main/java/com/metehanbolat/composebottomnavigationwithbadges/MainActivity.kt
+++ app/src/main/java/com/metehanbolat/composebottomnavigationwithbadges/MainActivity.kt
@@ -3,11 +3,18 @@ package com.metehanbolat.composebottomnavigationwithbadges
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import com.metehanbolat.composebottomnavigationwithbadges.ui.theme.ComposeBottomNavigationWithBadgesTheme
 
 class MainActivity : ComponentActivity() {
@@ -15,24 +22,53 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeBottomNavigationWithBadgesTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Navigation(navController: NavHostController) {
+    NavHost(navController = navController, startDestination = "home"){
+        composable("home"){
+            HomeScreen()
+        }
+        composable("chat"){
+            ChatScreen()
+        }
+        composable("settings"){
+            SettingsScreen()
+        }
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    ComposeBottomNavigationWithBadgesTheme {
-        Greeting("Android")
+fun HomeScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ){
+        Text(text = "Home Screen")
+    }
+}
+
+@Composable
+fun ChatScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ){
+        Text(text = "Chat Screen")
+    }
+}
+
+@Composable
+fun SettingsScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ){
+        Text(text = "Settings Screen")
     }
 }
